ff85375455126b647cfdf007e5ed274d758d88ba
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-03-06 17:55:04 +0000

    style: adjust nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,13 @@
-import { Newspaper } from "lucide-react";
+import { CirclePlus } from "lucide-react";
 import { Button } from "./ui/button";
 
 export default function Nav() {
   return (
-    <nav className="absolute top-0 flex w-full items-center justify-between px-12 py-6">
+    <nav className="sticky top-0 flex w-full items-center justify-between border-b-2 border-b-border px-16 py-6 backdrop-blur-lg">
       <h1 className="text-xl font-bold">Cerne</h1>
 
       <Button className="flex items-center justify-center gap-2">
-        <Newspaper />
+        <CirclePlus />
         <p>Nova prova</p>
       </Button>
     </nav>
