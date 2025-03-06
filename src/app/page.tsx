3cab43c5c2079431e5c013398a600705e3bf263f
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-03-06 17:55:46 +0000

    feat: add preview component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Form from "@/components/Form";
+import Preview from "@/components/Preview";
+
 export default function HomePage() {
-  return <main></main>;
+  return (
+    <main className="mx-16 mb-12 mt-16 flex h-screen justify-center">
+      <Form />
+      <Preview />
+    </main>
+  );
 }
