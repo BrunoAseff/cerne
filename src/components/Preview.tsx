c41eb05860605f1f457326da235e97c469f99b10
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-03-07 13:03:14 +0000

    feat: add spacing in preview

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -6,7 +6,7 @@ export default function Preview() {
   const title = useHeader((state) => state.title);
 
   return (
-    <article className="border-1 aspect-[1/1.414] w-[50%] rounded-2xl border-border bg-white p-8">
+    <article className="aspect-[1/1.414] w-[50%] rounded-xl border-2 border-border bg-white px-8 pb-8 pt-16 text-3xl">
       <div
         className="text-center"
         dangerouslySetInnerHTML={{ __html: title }}
