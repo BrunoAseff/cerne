cd5fee413645d70db3e651625f91dfc036c46719
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-03-09 03:27:39 +0000

    feat: add print button

--- src/components/PreviewToolbar.tsx
+++ src/components/PreviewToolbar.tsx
@@ -1,4 +1,10 @@
-import { ArrowDownToLine, Fullscreen, ZoomIn, ZoomOut } from "lucide-react";
+import {
+  ArrowDownToLine,
+  Fullscreen,
+  Printer,
+  ZoomIn,
+  ZoomOut,
+} from "lucide-react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import { Button } from "./ui/button";
 
@@ -36,6 +42,18 @@ export default function PreviewToolbar() {
           </TooltipTrigger>
 
           <TooltipContent>Tela cheia</TooltipContent>
+        </Tooltip>
+      </li>
+
+      <li>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button className="mr-3" variant="ghost" size="icon">
+              <Printer size={22} />
+            </Button>
+          </TooltipTrigger>
+
+          <TooltipContent>Imprimir</TooltipContent>
         </Tooltip>
       </li>
       <li>
