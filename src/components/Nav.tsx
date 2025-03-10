abff1df2e62425c006f86d93f77d0664655a1596
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-03-10 12:56:28 +0000

    style: adjust navbar padding

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function Nav() {
   return (
-    <nav className="sticky top-0 z-50 flex w-full items-center justify-between border-b-2 border-b-border px-16 py-6 backdrop-blur-lg">
+    <nav className="sticky top-0 z-50 flex w-full items-center justify-between border-b-2 border-b-border px-6 py-6 backdrop-blur-lg md:px-16">
       <h1 className="text-xl font-semibold">Cerne</h1>
 
       <Button className="flex items-center justify-center gap-2">
