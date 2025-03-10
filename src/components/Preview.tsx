be49ec123389fd8538678a39b397669011f61219
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-03-10 13:03:29 +0000

    style: make preview more responsive

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -6,7 +6,7 @@ export default function Preview() {
   const title = useHeader((state) => state.title);
 
   return (
-    <article className="aspect-[1/1.414] w-[50%] rounded-xl border-2 border-border bg-muted px-8 pb-8 pt-16 text-3xl">
+    <article className="aspect-[1/1.414] w-[150%] rounded-xl border-2 border-border bg-muted px-8 pb-8 pt-16 text-3xl md:w-[50%]">
       <div
         className="text-center"
         dangerouslySetInnerHTML={{ __html: title }}
