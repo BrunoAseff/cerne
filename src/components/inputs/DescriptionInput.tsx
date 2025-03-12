7b3a9889c30c99c8a988b9de6c55fcf78d4072c5
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-03-12 14:28:03 +0000

    feat: add section six

---
+++ src/components/inputs/DescriptionInput.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import "@/components/minimal-tiptap/styles/index.css";
+
+import type { Content, Editor } from "@tiptap/react";
+import type { UseMinimalTiptapEditorProps } from "@/components/minimal-tiptap/hooks/use-minimal-tiptap";
+import { EditorContent } from "@tiptap/react";
+import { cn } from "@/lib/utils";
+import { useMinimalTiptapEditor } from "@/components/minimal-tiptap/hooks/use-minimal-tiptap";
+import { MeasuredContainer } from "../minimal-tiptap/components/measured-container";
+import SectionSix from "../minimal-tiptap/components/section/six";
+
+export interface MinimalTiptapProps
+  extends Omit<UseMinimalTiptapEditorProps, "onUpdate"> {
+  value?: Content;
+  onChange?: (value: Content) => void;
+  className?: string;
+  editorContentClassName?: string;
+}
+
+const Toolbar = ({ editor }: { editor: Editor }) => (
+  <div className="shrink-0 overflow-x-auto border-t border-border p-2">
+    <div className="flex w-max items-center gap-px">
+      <SectionSix
+        editor={editor}
+        activeActions={["bold", "italic", "underline", "strikethrough"]}
+        mainActionCount={5}
+      />
+    </div>
+  </div>
+);
+
+export const DescriptionInput = React.forwardRef<
+  HTMLDivElement,
+  MinimalTiptapProps
+>(({ value, onChange, className, editorContentClassName, ...props }, ref) => {
+  const editor = useMinimalTiptapEditor({
+    value,
+    onUpdate: onChange,
+    ...props,
+  });
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <MeasuredContainer
+      as="div"
+      name="editor"
+      ref={ref}
+      className={cn(
+        "flex h-auto min-h-28 w-full flex-col rounded-md border border-input shadow-sm focus-within:border-primary",
+        className,
+      )}
+    >
+      <EditorContent
+        editor={editor}
+        className={cn("minimal-tiptap-editor", editorContentClassName)}
+      />
+      <Toolbar editor={editor} />
+    </MeasuredContainer>
+  );
+});
+
+DescriptionInput.displayName = "DescriptionInput";
+
+export default DescriptionInput;
